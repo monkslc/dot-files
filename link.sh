fbbe54d926edcb465776dc637c9174e1a3123be1
Connor Monks <monkslc@gmail.com>
2020-04-28 11:28:26 -0400

    add .vim folder to link script

--- link.sh
+++ link.sh
@@ -2,5 +2,6 @@ ln -s ~/dev/dot-files/.alacritty.yml ~/.alacritty.yml
 ln -s ~/dev/dot-files/.bash_profile ~/.bash_profile
 ln -s ~/dev/dot-files/.bashrc ~/.bashrc
 ln -s ~/dev/dot-files/.tmux.conf ~/.tmux.conf
+! ls ~/.vim && ln -s ~/dev/dot-files/.vim ~/.vim
 
 ln -s ~/dev/dot-files/starship.toml ~/.config/starship.toml
