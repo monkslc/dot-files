82beff7666c7c9e5011185ad178cfb3734965cb6
Connor Monks <monkslc@gmail.com>
2020-04-28 11:16:50 -0400

    restructure dot-files dir

---
+++ link.sh
@@ -0,0 +1,6 @@
+ln -s ~/dev/dot-files/.alacritty.yml ~/.alacritty.yml
+ln -s ~/dev/dot-files/.bash_profile ~/.bash_profile
+ln -s ~/dev/dot-files/.bashrc ~/.bashrc
+ln -s ~/dev/dot-files/.tmux.conf ~/.tmux.conf
+
+ln -s ~/dev/dot-files/starship.toml ~/.config/starship.toml
