5d3a1ddccd4ecda8662f51bbf00e601d422940c0
Connor Monks <monkslc@gmail.com>
2021-10-04 19:56:26 -0400

    move to sym link for .gitconfig file and make delta the diff tool

--- link.sh
+++ link.sh
@@ -2,17 +2,8 @@ ln -s ~/dev/dot-files/.alacritty.yml ~/.alacritty.yml
 ln -s ~/dev/dot-files/.bash_profile ~/.bash_profile
 ln -s ~/dev/dot-files/.bashrc ~/.bashrc
 ln -s ~/dev/dot-files/.tmux.conf ~/.tmux.conf
+ln -s ~/dev/dot-files/.gitconfig ~/.gitconfig
 ! ls ~/.vim && ln -s ~/dev/dot-files/.vim ~/.vim
 chsh -s /bin/bash
-
-git config --global core.excludesfile ~/dev/dot-files/.gitignore_global
-git config --replace-all --global alias.ds 'diff --staged'
-git config --replace-all --global alias.ss 'status -s'
-git config --replace-all --global alias.s 'status'
-git config --replace-all --global alias.d 'diff'
-git config --replace-all --global alias.a 'add'
-git config --replace-all --global alias.p 'pull'
-git config --replace-all --global alias.c 'commit'
-git config --replace-all --global alias.g 'log --oneline --abbrev-commit --all --graph --decorate --color'
 
 echo -e "\033[00;32mLinkage completed.\033[0m"
